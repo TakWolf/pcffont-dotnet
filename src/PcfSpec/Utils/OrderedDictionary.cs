f4967d51dda97fb8b9cebea59f2c2ae826d8dd53
TakWolf <takwolf@foxmail.com>
2026-06-10 23:48:56 +0800

    优化 OrderedDictionary 性能

--- src/PcfSpec/Utils/OrderedDictionary.cs
+++ src/PcfSpec/Utils/OrderedDictionary.cs
@@ -3,48 +3,61 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace PcfSpec.Utils;
 
-internal class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IList<KeyValuePair<TKey, TValue>>
+internal class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IList<KeyValuePair<TKey, TValue>> where TKey : notnull
 {
-    private readonly List<TKey> _keysData;
-    private readonly List<TValue> _valuesData;
+    private readonly List<KeyValuePair<TKey, TValue>> _items;
+    private readonly Dictionary<TKey, int> _indices;
+    private readonly IEqualityComparer<TKey> _comparer;
     private KeyCollection? _keys;
     private ValueCollection? _values;
 
-    public OrderedDictionary() : this(0) { }
+    public OrderedDictionary() : this(0, null) { }
 
-    public OrderedDictionary(int capacity)
+    public OrderedDictionary(int capacity) : this(capacity, null) { }
+
+    public OrderedDictionary(IEqualityComparer<TKey> comparer) : this(0, comparer) { }
+
+    public OrderedDictionary(int capacity, IEqualityComparer<TKey>? comparer)
     {
-        _keysData = new List<TKey>(capacity);
-        _valuesData = new List<TValue>(capacity);
+        _items = new List<KeyValuePair<TKey, TValue>>(capacity);
+        _indices = new Dictionary<TKey, int>(capacity, comparer);
+        _comparer = comparer ?? EqualityComparer<TKey>.Default;
     }
 
-    public OrderedDictionary(IDictionary<TKey, TValue> dictionary) : this(dictionary.Count)
+    public OrderedDictionary(IDictionary<TKey, TValue> dictionary) : this(dictionary, null) { }
+
+    public OrderedDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey>? comparer) : this(dictionary.Count, comparer)
     {
         foreach (var (key, value) in dictionary)
         {
-            this[key] = value;
+            Add(key, value);
         }
     }
 
-    public int Count => _keysData.Count;
+    public OrderedDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection) : this(collection, null) { }
+
+    public OrderedDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey>? comparer) :
+        this((collection as ICollection<KeyValuePair<TKey, TValue>>)?.Count ?? 0, comparer)
+    {
+        foreach (var (key, value) in collection)
+        {
+            Add(key, value);
+        }
+    }
+
+    public int Count => _items.Count;
 
     bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly => false;
 
-    public KeyCollection Keys => _keys ??= new KeyCollection(_keysData);
+    public KeyCollection Keys => _keys ??= new KeyCollection(_items, _indices);
 
     ICollection<TKey> IDictionary<TKey, TValue>.Keys => Keys;
 
-    public ValueCollection Values => _values ??= new ValueCollection(_valuesData);
+    public ValueCollection Values => _values ??= new ValueCollection(_items);
 
     ICollection<TValue> IDictionary<TKey, TValue>.Values => Values;
 
-    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-    {
-        for (var i = 0; i < _keysData.Count; i++)
-        {
-            yield return new KeyValuePair<TKey, TValue>(_keysData[i], _valuesData[i]);
-        }
-    }
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _items.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
@@ -52,163 +65,189 @@ internal class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ILis
     {
         get
         {
-            var index = _keysData.IndexOf(key);
-            if (index >= 0)
+            if (_indices.TryGetValue(key, out var index))
             {
-                return _valuesData[index];
+                return _items[index].Value;
             }
             throw new KeyNotFoundException($"The given key '{key}' was not present in the dictionary.");
         }
         set
         {
-            var index = _keysData.IndexOf(key);
-            if (index >= 0)
+            if (_indices.TryGetValue(key, out var index))
             {
-                _valuesData[index] = value;
+                var oldKey = _items[index].Key;
+                _items[index] = new KeyValuePair<TKey, TValue>(oldKey, value);
             }
             else
             {
-                _keysData.Add(key);
-                _valuesData.Add(value);
+                _indices.Add(key, _items.Count);
+                _items.Add(new KeyValuePair<TKey, TValue>(key, value));
             }
         }
     }
 
     KeyValuePair<TKey, TValue> IList<KeyValuePair<TKey, TValue>>.this[int index]
     {
-        get => new(_keysData[index], _valuesData[index]);
+        get => _items[index];
         set
         {
-            var existsIndex = _keysData.IndexOf(value.Key);
-            if (existsIndex >= 0 && existsIndex != index)
+            var newKey = value.Key;
+
+            if (_indices.TryGetValue(newKey, out var existsIndex) && existsIndex != index)
             {
-                throw new ArgumentException($"An element with the same key '{value.Key}' already exists in the dictionary.");
+                throw new ArgumentException($"An element with the same key '{newKey}' already exists in the dictionary.");
             }
-            _keysData[index] = value.Key;
-            _valuesData[index] = value.Value;
+
+            var oldKey = _items[index].Key;
+            _items[index] = value;
+
+            if (!_comparer.Equals(oldKey, newKey))
+            {
+                _indices.Remove(oldKey);
+                _indices[newKey] = index;
+            }
         }
     }
 
     public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        var index = _keysData.IndexOf(key);
-        if (index >= 0)
+        if (_indices.TryGetValue(key, out var index))
         {
-            value = _valuesData[index];
+            value = _items[index].Value;
             return true;
         }
         value = default;
         return false;
     }
 
-    public bool ContainsKey(TKey key) => _keysData.Contains(key);
+    public bool ContainsKey(TKey key) => _indices.ContainsKey(key);
 
-    public bool ContainsValue(TValue value) => _valuesData.Contains(value);
+    public bool ContainsValue(TValue value)
+    {
+        foreach (var item in _items)
+        {
+            if (EqualityComparer<TValue>.Default.Equals(item.Value, value))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
     {
-        var index = _keysData.IndexOf(item.Key);
-        if (index >= 0)
+        if (_indices.TryGetValue(item.Key, out var index))
         {
-            return EqualityComparer<TValue>.Default.Equals(_valuesData[index], item.Value);
+            return EqualityComparer<TValue>.Default.Equals(_items[index].Value, item.Value);
         }
         return false;
     }
 
     int IList<KeyValuePair<TKey, TValue>>.IndexOf(KeyValuePair<TKey, TValue> item)
     {
-        var index = _keysData.IndexOf(item.Key);
-        if (index >= 0)
+        if (_indices.TryGetValue(item.Key, out var index) && EqualityComparer<TValue>.Default.Equals(_items[index].Value, item.Value))
         {
-            return EqualityComparer<TValue>.Default.Equals(_valuesData[index], item.Value) ? index : -1;
+            return index;
         }
         return -1;
     }
 
     public void Add(TKey key, TValue value)
     {
-        if (_keysData.Contains(key))
+        if (_indices.ContainsKey(key))
         {
             throw new ArgumentException($"An element with the same key '{key}' already exists in the dictionary.");
         }
-        _keysData.Add(key);
-        _valuesData.Add(value);
+        _indices.Add(key, _items.Count);
+        _items.Add(new KeyValuePair<TKey, TValue>(key, value));
     }
 
     void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
 
     void IList<KeyValuePair<TKey, TValue>>.Insert(int index, KeyValuePair<TKey, TValue> item)
     {
-        if (_keysData.Contains(item.Key))
+        if (_indices.ContainsKey(item.Key))
         {
             throw new ArgumentException($"An element with the same key '{item.Key}' already exists in the dictionary.");
         }
-        _keysData.Insert(index, item.Key);
-        _valuesData.Insert(index, item.Value);
+        _items.Insert(index, item);
+        RebuildIndicesFrom(index);
     }
 
     public bool Remove(TKey key)
     {
-        var index = _keysData.IndexOf(key);
-        if (index >= 0)
+        if (!_indices.TryGetValue(key, out var index))
         {
-            _keysData.RemoveAt(index);
-            _valuesData.RemoveAt(index);
-            return true;
+            return false;
         }
-        return false;
+        _items.RemoveAt(index);
+        _indices.Remove(key);
+        RebuildIndicesFrom(index);
+        return true;
     }
 
     bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
     {
-        var index = _keysData.IndexOf(item.Key);
-        if (index >= 0 && EqualityComparer<TValue>.Default.Equals(_valuesData[index], item.Value))
+        if (!_indices.TryGetValue(item.Key, out var index) || !EqualityComparer<TValue>.Default.Equals(_items[index].Value, item.Value))
         {
-            _keysData.RemoveAt(index);
-            _valuesData.RemoveAt(index);
-            return true;
+            return false;
         }
-        return false;
+        _items.RemoveAt(index);
+        _indices.Remove(item.Key);
+        RebuildIndicesFrom(index);
+        return true;
     }
 
     void IList<KeyValuePair<TKey, TValue>>.RemoveAt(int index)
     {
-        _keysData.RemoveAt(index);
-        _valuesData.RemoveAt(index);
+        var key = _items[index].Key;
+        _items.RemoveAt(index);
+        _indices.Remove(key);
+        RebuildIndicesFrom(index);
     }
 
     public void Clear()
     {
-        _keysData.Clear();
-        _valuesData.Clear();
+        _items.Clear();
+        _indices.Clear();
     }
 
-    void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+
+    private void RebuildIndicesFrom(int startIndex)
     {
-        foreach (var pair in this)
+        for (var index = startIndex; index < _items.Count; index++)
         {
-            array[arrayIndex++] = pair;
+            _indices[_items[index].Key] = index;
         }
     }
 
     public class KeyCollection : ICollection<TKey>
     {
-        private readonly List<TKey> _keysData;
+        private readonly List<KeyValuePair<TKey, TValue>> _items;
+        private readonly Dictionary<TKey, int> _indices;
 
-        public KeyCollection(List<TKey> keysData)
+        public KeyCollection(List<KeyValuePair<TKey, TValue>> items, Dictionary<TKey, int> indices)
         {
-            _keysData = keysData;
+            _items = items;
+            _indices = indices;
         }
 
-        public int Count => _keysData.Count;
+        public int Count => _items.Count;
 
         bool ICollection<TKey>.IsReadOnly => true;
 
-        public IEnumerator<TKey> GetEnumerator() => _keysData.GetEnumerator();
+        public IEnumerator<TKey> GetEnumerator()
+        {
+            foreach (var item in _items)
+            {
+                yield return item.Key;
+            }
+        }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public bool Contains(TKey item) => _keysData.Contains(item);
+        public bool Contains(TKey key) => _indices.ContainsKey(key);
 
         void ICollection<TKey>.Add(TKey item) => throw new NotSupportedException();
 
@@ -216,27 +255,49 @@ internal class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ILis
 
         void ICollection<TKey>.Clear() => throw new NotSupportedException();
 
-        public void CopyTo(TKey[] array, int arrayIndex) => _keysData.CopyTo(array, arrayIndex);
+        public void CopyTo(TKey[] array, int arrayIndex)
+        {
+            foreach (var item in _items)
+            {
+                array[arrayIndex++] = item.Key;
+            }
+        }
     }
 
     public class ValueCollection : ICollection<TValue>
     {
-        private readonly List<TValue> _valuesData;
+        private readonly List<KeyValuePair<TKey, TValue>> _items;
 
-        public ValueCollection(List<TValue> valuesData)
+        public ValueCollection(List<KeyValuePair<TKey, TValue>> items)
         {
-            _valuesData = valuesData;
+            _items = items;
         }
 
-        public int Count => _valuesData.Count;
+        public int Count => _items.Count;
 
         bool ICollection<TValue>.IsReadOnly => true;
 
-        public IEnumerator<TValue> GetEnumerator() => _valuesData.GetEnumerator();
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            foreach (var item in _items)
+            {
+                yield return item.Value;
+            }
+        }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public bool Contains(TValue item) => _valuesData.Contains(item);
+        public bool Contains(TValue value)
+        {
+            foreach (var item in _items)
+            {
+                if (EqualityComparer<TValue>.Default.Equals(item.Value, value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
         void ICollection<TValue>.Add(TValue item) => throw new NotSupportedException();
 
@@ -244,6 +305,12 @@ internal class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ILis
 
         void ICollection<TValue>.Clear() => throw new NotSupportedException();
 
-        public void CopyTo(TValue[] array, int arrayIndex) => _valuesData.CopyTo(array, arrayIndex);
+        public void CopyTo(TValue[] array, int arrayIndex)
+        {
+            foreach (var item in _items)
+            {
+                array[arrayIndex++] = item.Value;
+            }
+        }
     }
 }
