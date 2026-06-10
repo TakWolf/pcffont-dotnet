7be73906c519ee79bb38255ca9aac7e0981f4b83
TakWolf <takwolf@foxmail.com>
2026-06-10 10:34:04 +0800

    优先使用 Math.Min() 和 Math.Max()

--- src/PcfSpec/Utils/CalculateUtil.cs
+++ src/PcfSpec/Utils/CalculateUtil.cs
@@ -29,11 +29,11 @@ internal static class CalculateUtil
             {
                 if (metric.LeftSideBearing != 0 || metric.RightSideBearing != 0 || metric.CharacterWidth != 0 || metric.Ascent != 0 || metric.Descent != 0)
                 {
-                    minBounds.LeftSideBearing = short.Min(minBounds.LeftSideBearing, metric.LeftSideBearing);
-                    minBounds.RightSideBearing = short.Min(minBounds.RightSideBearing, metric.RightSideBearing);
-                    minBounds.CharacterWidth = short.Min(minBounds.CharacterWidth, metric.CharacterWidth);
-                    minBounds.Ascent = short.Min(minBounds.Ascent, metric.Ascent);
-                    minBounds.Descent = short.Min(minBounds.Descent, metric.Descent);
+                    minBounds.LeftSideBearing = Math.Min(minBounds.LeftSideBearing, metric.LeftSideBearing);
+                    minBounds.RightSideBearing = Math.Min(minBounds.RightSideBearing, metric.RightSideBearing);
+                    minBounds.CharacterWidth = Math.Min(minBounds.CharacterWidth, metric.CharacterWidth);
+                    minBounds.Ascent = Math.Min(minBounds.Ascent, metric.Ascent);
+                    minBounds.Descent = Math.Min(minBounds.Descent, metric.Descent);
                 }
                 minBounds.Attributes &= metric.Attributes;
             }
@@ -54,11 +54,11 @@ internal static class CalculateUtil
             {
                 if (metric.LeftSideBearing != 0 || metric.RightSideBearing != 0 || metric.CharacterWidth != 0 || metric.Ascent != 0 || metric.Descent != 0)
                 {
-                    maxBounds.LeftSideBearing = short.Max(maxBounds.LeftSideBearing, metric.LeftSideBearing);
-                    maxBounds.RightSideBearing = short.Max(maxBounds.RightSideBearing, metric.RightSideBearing);
-                    maxBounds.CharacterWidth = short.Max(maxBounds.CharacterWidth, metric.CharacterWidth);
-                    maxBounds.Ascent = short.Max(maxBounds.Ascent, metric.Ascent);
-                    maxBounds.Descent = short.Max(maxBounds.Descent, metric.Descent);
+                    maxBounds.LeftSideBearing = Math.Max(maxBounds.LeftSideBearing, metric.LeftSideBearing);
+                    maxBounds.RightSideBearing = Math.Max(maxBounds.RightSideBearing, metric.RightSideBearing);
+                    maxBounds.CharacterWidth = Math.Max(maxBounds.CharacterWidth, metric.CharacterWidth);
+                    maxBounds.Ascent = Math.Max(maxBounds.Ascent, metric.Ascent);
+                    maxBounds.Descent = Math.Max(maxBounds.Descent, metric.Descent);
                 }
                 maxBounds.Attributes |= metric.Attributes;
             }
